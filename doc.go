8b902e9444514456cf49643abade666b4b4d52f7
Hedzr Yeh <hedzrz@gmail.com>
2019-06-01 23:05:26 +0800

    bump to v0.2.19
    - codecov to 91%
    - gocov to 93.7%
    - reviewed all codes and fixed bugs
    - fixed: data racing on SetNx/Set, and config files reloaded
    - added: passwd input safely

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "0.2.17" // version name
+	Version = "0.2.19" // version name
 	// VersionInt const
-	VersionInt = 0x0002017 // using as
+	VersionInt = 0x0002019 // using as
 )

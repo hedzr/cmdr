c13871d54d40b45924a5b047496e7bba9bde1680
Hedzr Yeh <hedzrz@gmail.com>
2019-06-03 13:50:41 +0800

    bump to v0.2.21
    - daemon: fixed wrong workdir,logdir
    - daemon: add flags for `server stop`: -1, -3, ...
    - daemon: using forking systemd type now.
    - config: fixed `--config`
    - demo: add test/demo codes
    - refine the codes

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "0.2.19" // version name
+	Version = "0.2.21" // version name
 	// VersionInt const
-	VersionInt = 0x0002019 // using as
+	VersionInt = 0x000215 // using as
 )

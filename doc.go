f266f4743b12042e13fcc7ff8e6b9561e75995ae
Hedzr Yeh <hedzrz@gmail.com>
2019-07-28 07:46:00 +0800

    bump to v1.1.0

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "1.0.3" // version name
+	Version = "1.1.0" // version name
 	// VersionInt const
-	VersionInt = 0x010003 // using as
+	VersionInt = 0x010100 // using as
 )

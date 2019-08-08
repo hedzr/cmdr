d9da6ba2896e16c8f32990a7802046460ede9a21
Hedzr Yeh <hedzrz@gmail.com>
2019-08-08 16:44:11 +0800

    bump to v1.1.2
    - fixed: on `--update-sim`
    - fixed: go switch is not c++ like.
    - best hook point to set env-prefix
    - `GetUsedConfigSubDir()` returns absolute path (normalized)

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "1.1.1" // version name
+	Version = "1.1.2" // version name
 	// VersionInt const
-	VersionInt = 0x010101 // using as
+	VersionInt = 0x010102 // using as
 )

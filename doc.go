9fa720d315b6ba69a2d27ef35e32463ca9984cba
Hedzr Yeh <hedzrz@gmail.com>
2019-08-21 14:22:44 +0800

    bump to v1.1.5; added `daemon.IsRunningInDemonizedMode()`; added `flag.Float32Var()` and `flag.Float32()`

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "1.1.4" // version name
+	Version = "1.1.5" // version name
 	// VersionInt const
-	VersionInt = 0x010104 // using as
+	VersionInt = 0x010105 // using as
 )

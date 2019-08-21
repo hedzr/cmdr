bce9d6b57206e2397e0b71975209808c616b31f4
Hedzr Yeh <hedzrz@gmail.com>
2019-08-21 21:48:56 +0800

    split the fsnotify calls for nacl

---
+++ fs_wrap.go
@@ -0,0 +1,74 @@
+// +build linux,windows,freebsd,darwin,aix,arm_linux,netbsd,openbsd,plan9,solaris
+
+/*
+ * Copyright © 2019 Hedzr Yeh.
+ */
+
+package cmdr
+
+import (
+	"bufio"
+	"github.com/fsnotify/fsnotify"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+func fsWatcherRoutine(s *Options, configDir string, initWG *sync.WaitGroup) {
+	watcher, err := fsnotify.NewWatcher()
+	if err == nil {
+		defer watcher.Close()
+
+		eventsWG := &sync.WaitGroup{}
+		eventsWG.Add(1)
+		go fsWatchRunner(s, configDir, watcher, eventsWG)
+		_ = watcher.Add(configDir)
+		initWG.Done()   // done initializing the watch in this go routine, so the parent routine can move on...
+		eventsWG.Wait() // now, wait for event loop to end in this go-routine...
+	}
+}
+
+func fsWatchRunner(s *Options, configDir string, watcher *fsnotify.Watcher, eventsWG *sync.WaitGroup) {
+	defer func() {
+		eventsWG.Done()
+	}()
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			// ok == false: 'Events' channel is closed
+			if ok {
+				// log.Debugf("ooo | fsnotify.watcher |%v", event.String())
+				// currentConfigFile, _ := filepath.EvalSymlinks(filename)
+				// we only care about the config file with the following cases:
+				// 1 - if the config file was modified or created
+				// 2 - if the real path to the config file changed (eg: k8s ConfigMap replacement)
+				const writeOrCreateMask = fsnotify.Write | fsnotify.Create
+				if strings.HasPrefix(filepath.Clean(event.Name), configDir) &&
+					event.Op&writeOrCreateMask != 0 &&
+					(testCfgSuffix(event.Name)) {
+					file, err := os.Open(event.Name)
+					if err != nil {
+						log.Printf("ERROR: os.Open() returned %v\n", err)
+					} else {
+						err = s.mergeConfigFile(bufio.NewReader(file), path.Ext(event.Name))
+						if err != nil {
+							log.Printf("ERROR: os.Open() returned %v\n", err)
+						}
+						s.reloadConfig()
+						file.Close()
+					}
+				}
+			}
+
+		case err, ok := <-watcher.Errors:
+			if ok { // 'Errors' channel is not closed
+				// log.Printf("watcher error: %v\n", err)
+				log.Printf("Watcher error: %v\n", err)
+			}
+			return
+		}
+	}
+}
---
+++ fs_wrap_nacl_amd64p32.go
@@ -0,0 +1,15 @@
+// +build nacl,amd64p32
+
+/*
+ * Copyright © 2019 Hedzr Yeh.
+ */
+
+package cmdr
+
+func fsWatcherRoutine(s *Options, configDir string, initWG *sync.WaitGroup) {
+	initWG.Done() // done initializing the watch in this go routine, so the parent routine can move on...
+}
+
+func fsWatchRunner(s *Options, configDir string, watcher *fsnotify.Watcher, eventsWG *sync.WaitGroup) {
+	eventsWG.Done()
+}

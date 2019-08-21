3941530d80791be0e47c8c5dcc8cff4460f86677
Hedzr Yeh <hedzrz@gmail.com>
2019-08-21 22:25:18 +0800

    bump to v1.1.6
    - make cmdr work under go playground
    - for #2, remove the tail line while EnableHelpCommands is false

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "1.1.5" // version name
+	Version = "1.1.6" // version name
 	// VersionInt const
-	VersionInt = 0x010105 // using as
+	VersionInt = 0x010106 // using as
 )

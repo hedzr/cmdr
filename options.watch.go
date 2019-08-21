c6fa970502b1a1eb5820f6c62e678deaa5f32fb2
Hedzr Yeh <hedzrz@gmail.com>
2019-08-21 21:48:37 +0800

    extract fsnotify functions

--- options.watch.go
+++ options.watch.go
@@ -10,11 +10,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/BurntSushi/toml"
-	"github.com/fsnotify/fsnotify"
 	"gopkg.in/yaml.v2"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 	"path/filepath"
@@ -208,7 +206,7 @@ func (s *Options) visit(path string, f os.FileInfo, e error) (err error) {
 	return
 }
 
-func (s *Options) reloadConfig(e fsnotify.Event) {
+func (s *Options) reloadConfig() {
 	// log.Debugf("\n\nConfig file changed: %s\n", e.String())
 
 	defer rwlCfgReload.RUnlock()
@@ -222,64 +220,10 @@ func (s *Options) reloadConfig(e fsnotify.Event) {
 }
 
 func (s *Options) watchConfigDir(configDir string) {
-	initWG := sync.WaitGroup{}
+	initWG := &sync.WaitGroup{}
 	initWG.Add(1)
-	go func() {
-		watcher, err := fsnotify.NewWatcher()
-		if err == nil {
-			defer watcher.Close()
-
-			eventsWG := &sync.WaitGroup{}
-			eventsWG.Add(1)
-			go s.watchRunner(configDir, watcher, eventsWG)
-			_ = watcher.Add(configDir)
-			initWG.Done()   // done initializing the watch in this go routine, so the parent routine can move on...
-			eventsWG.Wait() // now, wait for event loop to end in this go-routine...
-		}
-	}()
+	go fsWatcherRoutine(s, configDir, initWG)
 	initWG.Wait() // make sure that the go routine above fully ended before returning
-}
-
-func (s *Options) watchRunner(configDir string, watcher *fsnotify.Watcher, eventsWG *sync.WaitGroup) {
-	defer func() {
-		eventsWG.Done()
-	}()
-	for {
-		select {
-		case event, ok := <-watcher.Events:
-			// ok == false: 'Events' channel is closed
-			if ok {
-				// log.Debugf("ooo | fsnotify.watcher |%v", event.String())
-				// currentConfigFile, _ := filepath.EvalSymlinks(filename)
-				// we only care about the config file with the following cases:
-				// 1 - if the config file was modified or created
-				// 2 - if the real path to the config file changed (eg: k8s ConfigMap replacement)
-				const writeOrCreateMask = fsnotify.Write | fsnotify.Create
-				if strings.HasPrefix(filepath.Clean(event.Name), configDir) &&
-					event.Op&writeOrCreateMask != 0 &&
-					(testCfgSuffix(event.Name)) {
-					file, err := os.Open(event.Name)
-					if err != nil {
-						log.Printf("ERROR: os.Open() returned %v\n", err)
-					} else {
-						err = s.mergeConfigFile(bufio.NewReader(file), path.Ext(event.Name))
-						if err != nil {
-							log.Printf("ERROR: os.Open() returned %v\n", err)
-						}
-						s.reloadConfig(event)
-						file.Close()
-					}
-				}
-			}
-
-		case err, ok := <-watcher.Errors:
-			if ok { // 'Errors' channel is not closed
-				// log.Printf("watcher error: %v\n", err)
-				log.Printf("Watcher error: %v\n", err)
-			}
-			return
-		}
-	}
 }
 
 func testCfgSuffix(name string) bool {

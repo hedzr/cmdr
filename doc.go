3c33c99959af25f616938e98c29aa024a78c6232
Hedzr Yeh <hedzrz@gmail.com>
2019-08-31 21:21:40 +0800

    bumped to v1.1.7: enh/daemon signals, enh/TrapSignals; new/nacl supports; imp+fix/examples; imp/doc;

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@ const (
 	// AppName const
 	AppName = "cmdr" // main app-name
 	// Version const
-	Version = "1.1.6" // version name
+	Version = "1.1.7" // version name
 	// VersionInt const
-	VersionInt = 0x010106 // using as
+	VersionInt = 0x010107 // using as
 )

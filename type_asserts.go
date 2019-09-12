73713a721a12ed5c22265129d2b89f6946c3e139
Hedzr Yeh <hedzrz@gmail.com>
2019-09-12 11:33:59 +0800

    split type_asserts

---
+++ type_asserts.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2019 Hedzr Yeh.
+ */
+
+package cmdr
+
+import "reflect"
+
+// func isTypeInt(kind reflect.Kind) bool {
+// 	switch kind {
+// 	case reflect.Int:
+// 	case reflect.Int8:
+// 	case reflect.Int16:
+// 	case reflect.Int32:
+// 	case reflect.Int64:
+// 	case reflect.Uint:
+// 	case reflect.Uint8:
+// 	case reflect.Uint16:
+// 	case reflect.Uint32:
+// 	case reflect.Uint64:
+// 	default:
+// 		return false
+// 	}
+// 	return true
+// }
+
+func isTypeUint(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return true
+	default:
+		return false
+	}
+}
+
+func isTypeSInt(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return true
+	default:
+		return false
+	}
+}
+
+func isBool(v interface{}) bool {
+	_, ok := v.(bool)
+	return ok
+}
+
+func isNil1(v interface{}) bool {
+	return v == nil
+}

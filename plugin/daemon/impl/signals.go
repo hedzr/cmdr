8f3c109fba2db9314ac8f4e37307a1363a1f3eb2
Hedzr Yeh <hedzrz@gmail.com>
2019-10-09 20:46:20 +0800

    signals
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- plugin/daemon/impl/signals.go
+++ plugin/daemon/impl/signals.go
@@ -9,37 +9,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"syscall"
 )
-
-// // SetupSignals initialize all signal handlers
-// func SetupSignals() {
-// 	setupSignals()
-// }
-
-func setupSignals() {
-	// for i := 1; i < 34; i++ {
-	// 	daemon.SetSigHandler(termHandler, syscall.Signal(i))
-	// }
-
-	signals := []os.Signal{syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGABRT, syscall.SIGINT, syscall.SIGKILL, syscall.SIGUSR1, syscall.SIGUSR2}
-	if onSetTermHandler != nil {
-		signals = onSetTermHandler()
-	}
-	SetSigHandler(termHandler, signals...)
-
-	signals = []os.Signal{syscall.Signal(0x7)}
-	if onSetSigEmtHandler != nil {
-		signals = onSetSigEmtHandler()
-	}
-	SetSigHandler(sigEmtHandler, signals...)
-
-	signals = []os.Signal{syscall.SIGHUP}
-	if onSetReloadHandler != nil {
-		signals = onSetReloadHandler()
-	}
-	SetSigHandler(reloadHandler, signals...)
-}
 
 // ServeSignals calls handlers for system signals.
 // before invoking ServeSignals(), you should run SetupSignals() at first.
@@ -51,15 +21,13 @@ func ServeSignals() (err error) {
 		return // no handlers, skip the os signal listening
 	}
 
-	signals := make([]os.Signal, 0, len(handlers))
-	for sig := range handlers {
-		signals = append(signals, sig)
-	}
+	// syscall.Getenv()
+	signals := makeHandlers()
 
 	defer func() {
 		removePID()
 	}()
-	
+
 	ch := make(chan os.Signal, 8)
 	signal.Notify(ch, signals...)
 
@@ -89,27 +57,6 @@ func HandleSignalCaughtEvent() bool {
 	default:
 		return false
 	}
-}
-
-func termHandler(sig os.Signal) error {
-	log.Println("terminating...")
-	stop <- struct{}{}
-	log.Println("  - send stop ch")
-	if sig == syscall.SIGQUIT {
-		log.Println("  - waiting for done ch...")
-		<-done
-		log.Println("  - done ch received.")
-	}
-	return ErrStop
-}
-
-func sigEmtHandler(sig os.Signal) error {
-	log.Println("terminating (SIGEMT)...")
-	stop <- struct{}{}
-	if sig == syscall.SIGQUIT {
-		<-done
-	}
-	return ErrStop
 }
 
 func reloadHandler(sig os.Signal) error {

f8e05f8a3516c13593c1dcc7c39f180e856fa799
Hedzr Yeh <hedzrz@gmail.com>
2019-10-09 20:46:58 +0800

    fork
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- plugin/daemon/impl/fork.go
+++ plugin/daemon/impl/fork.go
@@ -41,7 +41,7 @@ func forkDaemon() (err error) {
 		Env:   os.Environ(),
 		Files: fds(os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()),
 	}
-	pid, err = syscall.ForkExec(os.Args[0], os.Args, procAttr)
+	pid, _, err = syscall.StartProcess(os.Args[0], os.Args, procAttr)
 	if err != nil {
 		log.Printf("Fork failed: %+v", err)
 		os.Exit(ErrnoForkAndDaemonFailed)
@@ -52,34 +52,4 @@ func forkDaemon() (err error) {
 	log.Printf("child process is running and detached at: %v", pid)
 	os.Exit(0)
 	return
-}
-
-func detachFromTty(nochdir, noclose bool) {
-	/* Change the file mode mask */
-	_ = syscall.Umask(0)
-
-	// create a new SID for the child process
-	s_ret, s_errno := syscall.Setsid()
-	if s_errno != nil {
-		log.Printf("Error: syscall.Setsid errno: %d", s_errno)
-		os.Exit(ErrnoForkAndDaemonFailed)
-	}
-	if s_ret < 0 {
-		log.Printf("Error: syscall.Setsid s_ret: %d", s_ret)
-		os.Exit(ErrnoForkAndDaemonFailed)
-	}
-	if !nochdir {
-		s_errno = os.Chdir("/")
-	}
-
-	if !noclose {
-		fds := fds(0, 0, 0)
-		s_errno = syscall.Dup2(int(fds[0]), int(os.Stdin.Fd()))
-		if s_errno == nil {
-			s_errno = syscall.Dup2(int(fds[1]), int(os.Stdout.Fd()))
-		}
-		if s_errno == nil {
-			s_errno = syscall.Dup2(int(fds[2]), int(os.Stderr.Fd()))
-		}
-	}
 }

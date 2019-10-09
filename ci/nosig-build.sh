46173f31f21fb60880a39ef850a7c68909c69c58
Hedzr Yeh <hedzrz@gmail.com>
2019-10-10 00:01:10 +0800

    nosig full builds script
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- ci/nosig-build.sh
+++ ci/nosig-build.sh
@@ -2,18 +2,59 @@
 
 # aix darwin dragonfly freebsd js,wasm linux nacl netbsd openbsd solaris
 
-build-all(){
+# go tool dist list
+
+build-all-full() {
+  for xx in $(go tool dist list); do
+    IFS='/' read -r -a array <<<"$xx"
+
+    case ${array[0]} in
+    aix | android | js) ;;
+
+    darwin | linux)
+      case ${array[1]} in
+      arm*) ;;
+      *)
+        local dist="${array[0]}-${array[1]}"
+        local GOOS=${array[0]}
+        local GOARCH=${array[1]}
+        echo $dist
+        one
+        ;;
+      esac
+      ;;
+
+    *)
+      local dist="${array[0]}-${array[1]}"
+      local GOOS=${array[0]}
+      local GOARCH=${array[1]}
+      echo $dist
+      one
+      ;;
+    esac
+  done
+}
+
+one() {
+  echo "-- build for $GOOS - $GOARCH ..."
+  GOOS=$GOOS GOARCH=$GOARCH go build -v -o bin/nosig-$GOOS-$GOARCH plugin/daemon/nosig/main.go
+  ls -la bin/nosig-$GOOS-$GOARCH
+  echo -n " > file $GOOS - $GOARCH ..."
+  file bin/nosig-$GOOS-$GOARCH
+}
+
+build-all() {
   local A=(darwin
-  dragonfly
-  freebsd
-  linux
-  nacl
-  netbsd
-  openbsd
-  plan9
-  solaris
-  windows)
-  
+    dragonfly
+    freebsd
+    linux
+    nacl
+    netbsd
+    openbsd
+    plan9
+    solaris
+    windows)
+
   for GOOS in "${A[@]}"; do
     if [ "$GOOS" == "nacl" ]; then
       local B=(386 amd64p32 arm)
@@ -25,9 +66,9 @@ build-all(){
       GOOS=$GOOS GOARCH=$GOARCH go build -v -o bin/nosig-$GOOS-$GOARCH plugin/daemon/nosig/main.go
     done
   done
-  
+
   ls -la bin/nosig*
-  
+
   for GOOS in "${A[@]}"; do
     if [ "$GOOS" == "nacl" ]; then
       local B=(386 amd64p32 arm)
@@ -57,5 +98,11 @@ build-one() {
   done
 }
 
-cmd=$1 && shift || { build-all; exit; }
-build-one $cmd
+cmd=$1 && shift || {
+  build-all
+  exit
+}
+case $cmd in
+full) build-all-full ;;
+*) build-one $cmd ;;
+esac

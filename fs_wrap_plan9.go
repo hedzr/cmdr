281d1e6f06990253dda9ef6e9a6f11d9e22ba995
Hedzr Yeh <hedzrz@gmail.com>
2019-10-09 20:43:39 +0800

    fs_wrap: plan9
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

---
+++ fs_wrap_plan9.go
@@ -0,0 +1,17 @@
+// +build plan9
+
+// Copyright © 2019 Hedzr Yeh.
+
+package cmdr
+
+import (
+	"sync"
+)
+
+func fsWatcherRoutine(s *Options, configDir string, initWG *sync.WaitGroup) {
+	initWG.Done() // done initializing the watch in this go routine, so the parent routine can move on...
+}
+
+// func fsWatchRunner(s *Options, configDir string, watcher *fsnotify.Watcher, eventsWG *sync.WaitGroup) {
+// 	eventsWG.Done()
+// }

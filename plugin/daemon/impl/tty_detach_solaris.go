33b1cd9e56b6693858ec1672d26eb7efd70fd446
Hedzr Yeh <hedzrz@gmail.com>
2019-10-09 20:46:34 +0800

    tty
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

---
+++ plugin/daemon/impl/tty_detach_solaris.go
@@ -0,0 +1,41 @@
+// Copyright © 2019 Hedzr Yeh.
+
+package impl
+
+import (
+	"log"
+	"os"
+	"syscall"
+)
+
+func detachFromTty(nochdir, noclose bool) {
+	/* Change the file mode mask */
+	_ = syscall.Umask(0)
+
+	// create a new SID for the child process
+	s_ret, s_errno := syscall.Setsid()
+	if s_errno != nil {
+		log.Printf("Error: syscall.Setsid errno: %d", s_errno)
+		os.Exit(ErrnoForkAndDaemonFailed)
+	}
+	if s_ret < 0 {
+		log.Printf("Error: syscall.Setsid s_ret: %d", s_ret)
+		os.Exit(ErrnoForkAndDaemonFailed)
+	}
+	if !nochdir {
+		s_errno = os.Chdir("/")
+	}
+
+	// TODO find the replacement for syscall.Dup2() in solaris
+	
+	// if !noclose {
+	// 	fds := fds(0, 0, 0)
+	// 	s_errno = syscall.Dup2(int(fds[0]), int(os.Stdin.Fd()))
+	// 	if s_errno == nil {
+	// 		s_errno = syscall.Dup2(int(fds[1]), int(os.Stdout.Fd()))
+	// 	}
+	// 	if s_errno == nil {
+	// 		s_errno = syscall.Dup2(int(fds[2]), int(os.Stderr.Fd()))
+	// 	}
+	// }
+}
---
+++ plugin/daemon/impl/tty_detach_windows.go
@@ -0,0 +1,9 @@
+// +build windows
+
+// Copyright © 2019 Hedzr Yeh.
+
+package impl
+
+func detachFromTty(nochdir, noclose bool) {
+}
+

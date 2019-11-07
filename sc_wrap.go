a3b9bd5fc8a9eee8d5a61c93608edc187cd9b73e
Hedzr Yeh <hedzrz@gmail.com>
2019-11-07 11:04:20 +0800

    better TrapSignalsEnh() and SignalXXX functions
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- sc_wrap.go
+++ sc_wrap.go
@@ -28,7 +28,30 @@ import (
 //
 //
 func TrapSignals(onTrapped func(s os.Signal), signals ...os.Signal) (waiter func()) {
-	done := make(chan struct{}, 1)
+	done := make(chan bool, 1)
+	waiter = TrapSignalsEnh(done, onTrapped)
+	return
+}
+
+// TrapSignals is a helper for simplify your infinite loop codes.
+//
+// Usage
+//
+//  func enteringLoop() {
+//    done := make(chan bool, 1)
+//    go func(done chan bool){
+//       // your main serve loop
+//       done <- true   // to end the TrapSignalsEnh waiter by manually, instead of os signals caught.
+//    }(done)
+// 	  waiter := cmdr.TrapSignalsEnh(done, func(s os.Signal) {
+// 	    logrus.Debugf("receive signal '%v' in onTrapped()", s)
+// 	  })
+// 	  go waiter()
+//  }
+//
+//
+//
+func TrapSignalsEnh(done chan bool, onTrapped func(s os.Signal), signals ...os.Signal) (waiter func()) {
 	sigs := make(chan os.Signal, 1)
 	if len(signals) == 0 {
 		signals = []os.Signal{syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGQUIT}
@@ -36,26 +59,57 @@ func TrapSignals(onTrapped func(s os.Signal), signals ...os.Signal) (waiter func
 	signal.Notify(sigs, signals...)
 
 	go func() {
-		s := <-sigs
-		logrus.Debugf("receive signal '%v'", s)
+		for {
+			select {
+			case s := <-sigs:
+				logrus.Debugf("receive signal '%v'", s)
 
-		onTrapped(s)
+				onTrapped(s)
 
-		// for _, s := range servers {
-		// 	s.Stop()
-		// }
-		// logrus.Infof("done")
-		done <- struct{}{}
+				// for _, s := range servers {
+				// 	s.Stop()
+				// }
+				// logrus.Infof("done")
+				done <- false
+				return
+			case <-done:
+				logrus.Debug("receive done sig and return for-select go-routine")
+				return
+			}
+		}
 	}()
 
 	waiter = func() {
 		for {
 			select {
-			case <-done:
+			case byManual := <-done:
+				if byManual {
+					done <- true // stop os signals for-select looper
+				}
 				return // os.Exit(1) // logrus.Infof("done got.")
 			}
 		}
 	}
 
 	return
+}
+
+// SignalToSelf trigger the sig signal to the current process
+func SignalToSelf(sig os.Signal) (err error) {
+	var p *os.Process
+	if p, err = os.FindProcess(os.Getpid()); err != nil {
+		logrus.Errorf("can't find process with pid=%v: %v", os.Getpid(), err)
+	}
+	err = p.Signal(sig)
+	return
+}
+
+// SignalQuitSignal post a SIGQUIT signal to the current process
+func SignalQuitSignal() {
+	_ = SignalToSelf(syscall.SIGQUIT)
+}
+
+// SignalTermSignal post a SIGTERM signal to the current process
+func SignalTermSignal() {
+	_ = SignalToSelf(syscall.SIGTERM)
 }

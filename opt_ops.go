6eeb408e52683ba2bf315599473529bd2b04d812
Hedzr Yeh <hedzrz@gmail.com>
2019-12-19 11:30:03 +0800

    These apis adds default value as parameter, such as `NewBool(bool)...` instead of `NewBool()`:
        - `NewBool(bool)`, `NewString(string)`,
        `NewStringSlice(defaultValue []string)`, `NewIntSlice(defaultValue []int)`,
        `NewInt(defaultValue int)`, `NewUint(defaultValue uint)`, `NewInt64(defaultValue int64)`, `NewUint64(defaultValue uint64)`, `NewFloat32(defaultValue float32)`, `NewFloat64(defaultValue float64)`,
        `NewDuration(defaultValue time.Duration)`,
    
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- opt_ops.go
+++ opt_ops.go
@@ -4,6 +4,8 @@
 
 package cmdr
 
+import "time"
+
 type (
 	// RootCmdOpt for fluent api
 	RootCmdOpt struct {
@@ -121,78 +123,99 @@ func NewSubCmd() (opt OptCmd) {
 }
 
 // NewBool for fluent api
-func NewBool() (opt OptFlag) {
+func NewBool(defaultValue bool) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &boolOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &boolOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewString for fluent api
-func NewString() (opt OptFlag) {
+func NewString(defaultValue string) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &stringOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &stringOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewStringSlice for fluent api
-func NewStringSlice() (opt OptFlag) {
+func NewStringSlice(defaultValue []string) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &stringSliceOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &stringSliceOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewIntSlice for fluent api
-func NewIntSlice() (opt OptFlag) {
+func NewIntSlice(defaultValue []int) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &intSliceOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &intSliceOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewInt for fluent api
-func NewInt() (opt OptFlag) {
+func NewInt(defaultValue int) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &intOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &intOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewUint for fluent api
-func NewUint() (opt OptFlag) {
+func NewUint(defaultValue uint) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &uintOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &uintOpt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewInt64 for fluent api
-func NewInt64() (opt OptFlag) {
+func NewInt64(defaultValue int64) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &int64Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &int64Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewUint64 for fluent api
-func NewUint64() (opt OptFlag) {
+func NewUint64(defaultValue uint64) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &uint64Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &uint64Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewFloat32 for fluent api
-func NewFloat32() (opt OptFlag) {
+func NewFloat32(defaultValue float32) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &float32Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &float32Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewFloat64 for fluent api
-func NewFloat64() (opt OptFlag) {
+func NewFloat64(defaultValue float64) (opt OptFlag) {
 	optCtx.workingFlag = &Flag{}
 	optCtx.current.Flags = append(optCtx.current.Flags, optCtx.workingFlag)
-	return &float64Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt = &float64Opt{optFlagImpl: optFlagImpl{working: optCtx.workingFlag}}
+	opt.DefaultValue(defaultValue, "")
+	return
 }
 
 // NewDuration for fluent api
-func NewDuration() (opt OptFlag) {
+func NewDuration(defaultValue time.Duration) (opt OptFlag) {
 	opt = NewDurationFrom(&Flag{})
+	opt.DefaultValue(defaultValue, "")
 	return
 }
 

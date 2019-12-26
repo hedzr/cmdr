8a8ba066141405a877bf4d198c1d0ea714ed829c
Hedzr Yeh <hedzrz@gmail.com>
2019-12-26 11:12:56 +0800

    doc
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- doc.go
+++ doc.go
@@ -2,6 +2,18 @@
  * Copyright © 2019 Hedzr Yeh.
  */
 
+/*
+
+Package cmdr interprets command-line arguments with POSIX style.
+
+cmdr has rich features:
+
+- builds multi-level command and sub-commands
+- builds short, long options with kinds of data types, aliases options too
+- defines commands and options via fluent api style
+- or defines its with enhanced stdlib `flag` style
+
+ */
 package cmdr
 
 const (

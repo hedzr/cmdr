b1342d5a18ed4e0e1faad82542d22fc889d0a9bf
Hedzr Yeh <hedzrz@gmail.com>
2020-04-11 11:46:31 +0800

    fixed: IsZero & build tags

--- clone_zero.go
+++ clone_zero.go
@@ -4,50 +4,54 @@
 
 package cmdr
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
 
 // IsZero reports whether v is the zero value for its type.
 // It panics if the argument is invalid.
-func IsZero(v Value) bool {
-	switch v.kind() {
-	case Bool:
+func IsZero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Bool:
 		return !v.Bool()
-	case Int, Int8, Int16, Int32, Int64:
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		return v.Int() == 0
-	case Uint, Uint8, Uint16, Uint32, Uint64, Uintptr:
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		return v.Uint() == 0
-	case Float32, Float64:
+	case reflect.Float32, reflect.Float64:
 		return math.Float64bits(v.Float()) == 0
-	case Complex64, Complex128:
+	case reflect.Complex64, reflect.Complex128:
 		c := v.Complex()
 		return math.Float64bits(real(c)) == 0 && math.Float64bits(imag(c)) == 0
-	case Array:
+	case reflect.Array:
 		return isZeroArray(v)
-	case Chan, Func, Interface, Map, Ptr, Slice, UnsafePointer:
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
 		return v.IsNil()
-	case String:
+	case reflect.String:
 		return v.Len() == 0
-	case Struct:
+	case reflect.Struct:
 		return isZeroStruct(v)
 	default:
 		// This should never happens, but will act as a safeguard for
 		// later, as a default value doesn't makes sense here.
-		panic(&ValueError{"reflect.Value.IsZero", v.Kind()})
+		panic(fmt.Sprintf("reflect.Value.IsZero, kind=%b", v.Kind()))
 	}
 }
 
-func isZeroArray(v Value) bool {
+func isZeroArray(v reflect.Value) bool {
 	for i := 0; i < v.Len(); i++ {
-		if !v.Index(i).IsZero() {
+		if !IsZero(v.Index(i)) {
 			return false
 		}
 	}
 	return true
 }
 
-func isZeroStruct(v Value) bool {
+func isZeroStruct(v reflect.Value) bool {
 	for i := 0; i < v.NumField(); i++ {
-		if !v.Field(i).IsZero() {
+		if !IsZero(v.Field(i)) {
 			return false
 		}
 	}
--- clone_zero_go1.13.go
+++ clone_zero_go1.13.go
@@ -1,6 +1,8 @@
 // Copyright © 2020 Hedzr Yeh.
 
-// + // build go113
+// +build go1.13
+
+// go1.14
 
 package cmdr
 
@@ -8,6 +10,10 @@ import "reflect"
 
 // IsZero reports whether v is the zero value for its type.
 // It panics if the argument is invalid.
-func IsZero(from reflect.Value) bool {
-	return from.IsZero()
+func IsZero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Bool:
+		break
+	}
+	return v.IsZero()
 }

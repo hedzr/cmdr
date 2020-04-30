4f56515b2eb9ee47fc36b7af73074f57942911cf
Hedzr Yeh <hedzrz@gmail.com>
2020-04-30 22:36:23 +0800

    some codes for plan9
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- fs_wrap_plan9.go
+++ fs_wrap_plan9.go
@@ -12,6 +12,19 @@ func fsWatcherRoutine(s *Options, configDir string, filesWatching []string, init
 	initWG.Done() // done initializing the watch in this go routine, so the parent routine can move on...
 }
 
-// func fsWatchRunner(s *Options, configDir string, watcher *fsnotify.Watcher, eventsWG *sync.WaitGroup) {
-// 	eventsWG.Done()
-// }
+func fsWatchRunner(s *Options, configDir string, watcher *fsnotify.Watcher, eventsWG *sync.WaitGroup) {
+	// eventsWG.Done()
+}
+
+
+// stopExitingChannelForFsWatcher stop fs watcher explicitly
+func stopExitingChannelForFsWatcher() {
+	effw.Lock()
+	defer effw.Unlock()
+	if cmdrExitingForFsWatcher != nil && GetBool("watching") {
+		cmdrExitingForFsWatcher <- struct{}{}
+	}
+}
+
+var cmdrExitingForFsWatcher = make(chan struct{}, 16)
+var effw sync.RWMutex

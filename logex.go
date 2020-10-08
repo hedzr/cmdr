f02751abd8c344d575d337f5e8db528717593fea
Hedzr Yeh <hedzrz@gmail.com>
2020-10-08 15:40:03 +0800

    added InDockerEnv()

--- logex.go
+++ logex.go
@@ -252,3 +252,22 @@ func inDevelopingTime() (status bool) {
 	}
 	return
 }
+
+// InDockerEnv detects whether is running within docker
+// container environment.
+func InDockerEnv() (status bool) {
+	return isRunningInDockerContainer()
+}
+
+func isRunningInDockerContainer() bool {
+	// docker creates a .dockerenv file at the root
+	// of the directory tree inside the container.
+	// if this file exists then the viewer is running
+	// from inside a container so return true
+
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		return true
+	}
+
+	return false
+}

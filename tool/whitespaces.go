dbd7f23a75e5436179b42794245937c3513c763b
Hedzr Yeh <hedzrz@gmail.com>
2022-01-23 14:03:36 +0800

    move and add IsTtyEscaped, StripEscapes

--- tool/whitespaces.go
+++ tool/whitespaces.go
@@ -8,6 +8,27 @@ import (
 	"strings"
 )
 
+// IsTtyEscaped detects a string if it contains ansi color escaped sequences
+func IsTtyEscaped(s string) bool { return isTtyEscaped(s) }
+func isTtyEscaped(s string) bool { return strings.Contains(s, "\x1b[") || strings.Contains(s, "\x9b[") }
+
+// StripEscapes removes any ansi color escaped sequences from a string
+func StripEscapes(str string) (strCleaned string) { return stripEscapes(str) }
+
+//var reStripEscapesOld = regexp.MustCompile(`\x1b\[[0-9,;]+m`)
+
+const ansi = "[\u001b\u009b][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+
+var reStripEscapes = regexp.MustCompile(ansi)
+
+func stripEscapes(str string) (strCleaned string) {
+	strCleaned = reStripEscapes.ReplaceAllString(str, "")
+	return
+}
+
+//// TrimQuotes strips first and last quote char (double quote or single quote).
+//func TrimQuotes(s string) string { return exec.TrimQuotes(s) }
+
 // StripQuotes strips single or double quotes around a string.
 func StripQuotes(s string) string {
 	return trimQuotes(s)

0820681ecb71b71344d7a5ff1781d17293ed77d5
Hedzr Yeh <hedzrz@gmail.com>
2022-02-11 15:01:34 +0800

    restore Match() but with new name MatchAndTest()

--- exec_match.go
+++ exec_match.go
@@ -6,6 +6,7 @@ package cmdr
 
 import (
 	"gopkg.in/hedzr/errors.v2"
+	"os"
 	"strings"
 )
 
@@ -250,5 +251,40 @@ func (w *ExecWorker) checkPrerequisitesForCmd(cc *Command, pkg *ptpkg) (err erro
 			return
 		}
 	}
+	return
+}
+
+// MatchAndTest _
+func MatchAndTest(inputCommandlineWithoutArg0 string, opts ...ExecOption) (last *Command, err error) {
+	return matchForTest(inputCommandlineWithoutArg0, opts...)
+}
+
+// match try parsing the input command-line, the result is the last hit *Command.
+func matchForTest(inputCommandlineWithoutArg0 string, opts ...ExecOption) (last *Command, err error) {
+	saved := internalGetWorker()
+	savedUnknownOptionHandler := unknownOptionHandler
+	rootCmd := saved.rootCommand
+
+	defer func() {
+		uniqueWorkerLock.Lock()
+		uniqueWorker = saved
+		unknownOptionHandler = savedUnknownOptionHandler
+		uniqueWorkerLock.Unlock()
+	}()
+
+	// get a new ExecWorker instance
+	w := internalResetWorkerNoLock()
+
+	for _, opt := range opts {
+		opt(w)
+	}
+
+	w.noDefaultHelpScreen = true
+	w.noUnknownCmdTip = true
+	w.noCommandAction = true
+	unknownOptionHandler = emptyUnknownOptionHandler
+
+	line := os.Args[0] + " " + inputCommandlineWithoutArg0
+	last, err = w.InternalExecFor(rootCmd, strings.Split(line, " "))
 	return
 }

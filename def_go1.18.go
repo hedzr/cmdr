7f35dbe2aa7ef5cb92bfb4d113a310e3b1c0f3df
Hedzr Yeh <hedzrz@gmail.com>
2022-02-16 11:45:19 +0800

    added Any, just in thinking, don't use it directly

---
+++ def_go1.18.go
@@ -0,0 +1,5 @@
+//go:build go1.18
+// +build go1.18
+
+package deepcopy
+
---
+++ def_older.go
@@ -0,0 +1,7 @@
+//go:build !go1.18
+// +build !go1.18
+
+package cmdr
+
+// Any type for go1.17 and older
+type Any interface{}

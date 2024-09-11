5a5d145725916539f9f12ec13ee38321c3b3f9e4
Hedzr Yeh <hedzrz@gmail.com>
2024-09-12 07:25:40 +0800

    remove deps in tests

--- builder/app_test.go
+++ builder/app_test.go
@@ -1,9 +1,9 @@
 package builder
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 
 	"github.com/hedzr/cmdr/v2/cli"
 )
@@ -13,27 +13,78 @@ func TestAppS_AddCmd(t *testing.T) {
 	a.AddCmd(func(b cli.CommandBuilder) {
 		b.Titles("ask", "a")
 	})
-	assert.Equal(t, a.root.Long, "ask")
-	assert.Equal(t, a.root.Short, "a")
+	assertEqual(t, a.root.Long, "ask")
+	assertEqual(t, a.root.Short, "a")
 
 	a.AddCmd(func(b cli.CommandBuilder) {
 		b.Titles("bunny", "b")
 	})
 	child := a.root.SubCommands()[0]
-	assert.Equal(t, child.Long, "bunny")
-	assert.Equal(t, child.Short, "b")
+	assertEqual(t, child.Long, "bunny")
+	assertEqual(t, child.Short, "b")
 }
 
 func TestAppS_Run(t *testing.T) {
 	a := &appS{inCmd: 1}
 	err := a.Run()
-	assert.Error(t, err)
+	assertEqual(t, err != nil, true)
 
 	a = &appS{inFlg: 2}
 	err = a.Run()
-	assert.Error(t, err)
+	assertEqual(t, err != nil, true)
 
 	a = &appS{}
 	err = a.Run()
-	assert.Equal(t, err, cli.ErrEmptyRootCommand)
+	assertEqual(t, err, cli.ErrEmptyRootCommand)
 }
+
+func assertEqual(t testing.TB, expect, actual any, msg ...any) { //nolint:govet,unparam //it's a printf/println dual interface
+	if reflect.DeepEqual(expect, actual) {
+		return
+	}
+
+	var mesg string
+	if len(msg) > 0 {
+		if format, ok := msg[0].(string); ok {
+			mesg = fmt.Sprintf(format, msg[1:]...)
+		} else {
+			mesg = fmt.Sprint(msg...)
+		}
+	}
+
+	t.Fatalf("assertEqual failed: %v\n    expect: %v\n    actual: %v\n", mesg, expect, actual)
+}
+
+// func assertTrue(t testing.TB, cond bool, msg ...any) { //nolint:revive
+// 	if cond {
+// 		return
+// 	}
+//
+// 	var mesg string
+// 	if len(msg) > 0 {
+// 		if format, ok := msg[0].(string); ok {
+// 			mesg = fmt.Sprintf(format, msg[1:]...)
+// 		} else {
+// 			mesg = fmt.Sprint(msg...)
+// 		}
+// 	}
+//
+// 	t.Fatalf("assertTrue failed: %s", mesg)
+// }
+//
+// func assertFalse(t testing.TB, cond bool, msg ...any) { //nolint:revive
+// 	if !cond {
+// 		return
+// 	}
+//
+// 	var mesg string
+// 	if len(msg) > 0 {
+// 		if format, ok := msg[0].(string); ok {
+// 			mesg = fmt.Sprintf(format, msg[1:]...)
+// 		} else {
+// 			mesg = fmt.Sprint(msg...)
+// 		}
+// 	}
+//
+// 	t.Fatalf("assertFalse failed: %s", mesg)
+// }

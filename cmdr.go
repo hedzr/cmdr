8a3ac040a222e8d202643410453ef912638fbf8b
Hedzr Yeh <hedzrz@gmail.com>
2024-10-09 18:31:55 +0800

    add more package-level functions

--- cmdr.go
+++ cmdr.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hedzr/cmdr/v2/builder"
 	"github.com/hedzr/cmdr/v2/cli"
 	"github.com/hedzr/cmdr/v2/cli/worker"
+	"github.com/hedzr/cmdr/v2/pkg/dir"
 )
 
 // func NewOpt[T any](defaultValue ...T) config.Opt {
@@ -24,28 +25,28 @@ import (
 //	app := cmdr.New().
 //	    Info("demo-app", "0.3.1").
 //	    Author("hedzr")
-//	app.AddCmd(func(b config.CommandBuilder) {
-//	    b.Titles("jump").
-//	        Description("jump command").
-//	        Examples(``).
-//	        Deprecated(``).
-//	        Hidden(false).
-//	        AddCmd(func(b config.CommandBuilder) {
-//	            b.Titles("to").
-//	                Description("to command").
-//	                Examples(``).
-//	                Deprecated(``).
-//	                Hidden(false).
-//	                OnAction(func(cmd *obj.Command, args []string) (err error) {
-//	                    return // handling command action here
-//	                }).
-//	                Build()
-//	        }).
-//	        Build()
-//	}).AddFlg(func(b config.FlagBuilder) {
-//	    b.Titles("dry-run", "n").Default(false).Build()
-//	})
-//
+//	app.Cmd("jump").
+//		Description("jump command").
+//		Examples(`jump example`).
+//		Deprecated(`jump is a demo command`).
+//		With(func(b cli.CommandBuilder) {
+//			b.Hidden(false)
+//			b.Cmd("to").
+//				Description("to command").
+//				Examples(``).
+//				Deprecated(`v0.1.1`).
+//				Hidden(false).
+//				OnAction(func(cmd *cli.Command, args []string) (err error) {
+//					main1()
+//					return // handling command action here
+//				}).
+//				With(func(b cli.CommandBuilder) {
+//					b.Flg("full", "f").
+//						Default(false).
+//						Description("full command").
+//						Build()
+//				})
+//		})
 //	app.Flg("dry-run", "n").
 //	    Default(false).
 //	    Build() // no matter even if you're adding the duplicated one.
@@ -82,6 +83,47 @@ func New(opts ...cli.Opt) cli.App {
 // It's available once New() / Exec() called, else nil.
 func App() cli.Runner { return worker.UniqueWorker() }
 
+func AppName() string            { return App().Name() }            // the app's name
+func AppVersion() string         { return App().Version() }         // the app's version
+func AppDescription() string     { return App().Root().Desc() }     // the app's short description
+func AppDescriptionLong() string { return App().Root().DescLong() } // the app's long description
+
+func CmdLines() []string { return append([]string{dir.GetExecutablePath()}, os.Args...) }
+
+// func Parsed() bool { return worker.UniqueWorker(). }
+
+// Store returns the KVStore associated with current App().
+func Store() store.Store { return App().Store() }
+
+// CmdStore returns the child Store at 'app.cmd'.
+// By default, cmdr maintains all command-line subcommands and flags
+// as a child tree in the associated Store internally.
+//
+// You can check out the flags state by querying in this child store.
+//
+// For example, we have a command 'server'->'start' and its
+// flag 'foreground', therefore we can query the flag what if
+// it was given by user's 'app server start --foreground':
+//
+//	fore := cmdr.CmdStore().MustBool("server.start.foreground", false)
+//	if fore {
+//	    runRealMain()
+//	} else {
+//	    service.Start("start", runRealMain) // start the real main as a service
+//	}
+//
+// Q: How to inspect the internal Store()?
+//
+// A: Running `app [any subcommands] [any options] ~~debug` will dump
+// the internal Store() tree.
+//
+// Q: Can I list all subcommands?
+//
+// A: Running `app ~~tree`, `app -v ~~tree` or `app ~~tree -vvv` can get
+// a list of subcommands tree, and with those builtin hidden commands,
+// and with those vendor hidden commands.
+func CmdStore() store.Store { return Store().WithPrefix("app.cmd") }
+
 // Exec starts a new cmdr app (parsing cmdline args based on the given rootCmd)
 // from scratch.
 //
@@ -89,7 +131,7 @@ func App() cli.Runner { return worker.UniqueWorker() }
 //
 // It'll be removed completely at the recently future version.
 //
-// Deprecated since 2.1
+// Deprecated since 2.1 by app.Run()
 func Exec(rootCmd *cli.RootCommand, opts ...cli.Opt) (err error) {
 	// if is.InDebugging() {
 	// 	_ = exec.Run("/bin/false")
@@ -114,6 +156,29 @@ func WithUnmatchedAsError(b bool) cli.Opt {
 	}
 }
 
+// WithStore gives a user-defined Store as initial, or by default
+// cmdr makes a dummy Store internally.
+//
+// So you must have a new Store to be transferred into cmdr if
+// you want integrating cmdr and fully-functional Store. Like this,
+//
+//		app := prepareApp()
+//		if err := app.Run(
+//			cmdr.WithStore(store.New()),        // create a standard Store instead of internal dummyStore
+//			// cmdr.WithExternalLoaders(
+//			// 	local.NewConfigFileLoader(),    // import "github.com/hedzr/cmdr-loaders/local" to get in advanced external loading features
+//			// 	local.NewEnvVarLoader(),
+//			// ),
+//			cmdr.WithForceDefaultAction(false), // true for debug in developing time
+//		); err != nil {
+//			logz.Error("Application Error:", "err", err)
+//		}
+//
+//	 func prepareApp() cli.App {
+//			app = cmdr.New().                   // the minimal app is `cmdr.New()`
+//				Info("tiny-app", "0.3.1").
+//				Author("example.com Authors")
+//		}
 func WithStore(conf store.Store) cli.Opt {
 	return func(s *cli.Config) {
 		s.Store = conf

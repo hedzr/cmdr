a2dd5e9ee67ba2171cbd93509bd52ef3ddfc3094
Hedzr Yeh <hedzrz@gmail.com>
2024-10-12 18:12:14 +0800

    improved tiny app

--- tiny/tiny/main.go
+++ tiny/tiny/main.go
@@ -1,20 +1,16 @@
 package main
 
-// normally tiny app
+// Simplest tiny app
 
 import (
 	"io"
-	"log"
 	"os"
-	"strings"
-	"sync"
 
 	"gopkg.in/hedzr/errors.v3"
 
 	"github.com/hedzr/cmdr/v2"
 	"github.com/hedzr/cmdr/v2/cli"
 	"github.com/hedzr/cmdr/v2/pkg/dir"
-	"github.com/hedzr/is"
 	logz "github.com/hedzr/logg/slog"
 	"github.com/hedzr/store"
 )
@@ -31,21 +27,12 @@ func main() {
 		cmdr.WithTasksBeforeRun(func(cmd *cli.Command, runner cli.Runner, extras ...any) (err error) {
 			logz.Debug("command running...", "cmd", cmd, "runner", runner, "extras", extras)
 			return
-		}), // cmdr.WithTasksBeforeParse(), cmdr.WithTasksBeforeRun(), cmdr.WithTasksAfterRun
+		}),
 
 		// true for debug in developing time, it'll disable onAction on each Cmd.
 		// for productive mode, comment this line.
 		cmdr.WithForceDefaultAction(true),
-
-		cmdr.WithSortInHelpScreen(true),
-		// cmdr.WithDontGroupInHelpScreen(true),
 	)
-
-	// // simple run the parser of app and trigger the matched command's action
-	// _ = app.Run(
-	// 	cmdr.WithForceDefaultAction(false), // true for debug in developing time
-	// )
-
 	if err := app.Run(); err != nil {
 		logz.Error("Application Error:", "err", err)
 		os.Exit(app.SuggestRetCode())
@@ -61,7 +48,8 @@ func prepareApp(opts ...cli.Opt) (app cli.App) {
 		Description("disable force default action").
 		OnMatched(func(f *cli.Flag, position int, hitState *cli.MatchState) (err error) {
 			if b, ok := hitState.Value.(bool); ok {
-				app.Store().Set("app.force-default-action", b) // disable/enable the final state about 'force default action'
+				// disable/enable the final state about 'force default action'
+				app.Store().Set("app.force-default-action", b)
 			}
 			return
 		}).
@@ -101,13 +89,11 @@ func prepareApp(opts ...cli.Opt) (app cli.App) {
 	app.Flg("dry-run", "n").
 		Default(false).
 		Description("run all but without committing").
-		Group(cli.UnsortedGroup).
 		Build()
 
 	app.Flg("wet-run", "w").
 		Default(false).
 		Description("run all but with committing").
-		// Group(cli.UnsortedGroup).
 		Build() // no matter even if you're adding the duplicated one.
 
 	app.Cmd("wrong").
@@ -116,59 +102,13 @@ func prepareApp(opts ...cli.Opt) (app cli.App) {
 			ec := errors.New()
 			defer ec.Defer(&err) // store the collected errors in native err and return it
 			ec.Attach(io.ErrClosedPipe, errors.New("something's wrong"), os.ErrPermission)
-			return // handling command action here
+			return
 		}).
 		With(func(b cli.CommandBuilder) {
 			b.Flg("full", "f").
 				Default(false).
 				Description("full command").
-				// Group(cli.UnsortedGroup).
 				Build()
 		})
 	return
-}
-
-var onceDev sync.Once
-var devMode bool
-
-func init() {
-	// onceDev is a redundant operation, but we still keep it to
-	// fit for defensive programming style.
-	onceDev.Do(func() {
-		log.SetFlags(log.LstdFlags | log.Lmsgprefix | log.LUTC | log.Lshortfile | log.Lmicroseconds)
-		log.SetPrefix("")
-
-		if dir.FileExists(".dev-mode") {
-			devMode = true
-		} else if dir.FileExists("go.mod") {
-			data, err := os.ReadFile("go.mod")
-			if err != nil {
-				return
-			}
-			content := string(data)
-
-			// dev := true
-			if strings.Contains(content, "github.com/hedzr/cmdr/v2/pkg/") {
-				devMode = false
-			}
-
-			// I am tiny-app in cmdr/v2, I will be launched in dev-mode always
-			if strings.Contains(content, "module github.com/hedzr/cmdr/v2") {
-				devMode = true
-			}
-		}
-
-		if devMode {
-			is.SetDebugMode(true)
-			logz.Debug(".dev-mode file detected, entering Debug Mode...")
-		}
-
-		if is.DebugBuild() {
-			is.SetDebugMode(true)
-		}
-
-		if is.VerboseBuild() {
-			is.SetVerboseMode(true)
-		}
-	})
 }

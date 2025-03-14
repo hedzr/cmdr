8e63d16478b3a0556e6eb8fb695c628f54d395e7
Hedzr Yeh <hedzrz@gmail.com>
2025-03-14 08:21:49 +0800

    add examples/cmd/preset-cmdline.go

---
+++ examples/cmd/preset-cmdline.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/hedzr/cmdr/v2/cli"
+)
+
+type presetCmd struct{}
+
+func (presetCmd) Add(app cli.App) {
+	app.Cmd("preset", "p").
+		Description("preset command").
+		With(func(b cli.CommandBuilder) {
+			b.Flg("preset", "p").
+				Default(false).
+				Description("preset arg").
+				Build()
+			b.Cmd("cmd", "c").
+				PresetCmdLines(`-pv`).
+				OnAction(func(ctx context.Context, cmd cli.Cmd, args []string) (err error) {
+					_, err = app.DoBuiltinAction(ctx, cli.ActionDefault)
+					return
+				}).Build()
+		})
+}

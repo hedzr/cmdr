1e0435843dc27f2004882b80509160c21fb43e20
Hedzr Yeh <hedzrz@gmail.com>
2025-03-16 07:08:07 +0800

    cmdr: add `TestStoreGetSectionFrom()`

--- cmdr_test.go
+++ cmdr_test.go
@@ -32,6 +32,78 @@ func TestDottedPathToCommandOrFlag(t *testing.T) {
 	}
 }
 
+func TestStoreGetSectionFrom(t *testing.T) {
+	ctx := context.Background()
+	app := cmdr.Create("app", "v1", `author`, `desc`,
+		cli.WithArgs("test-app", "--debug"),
+	).OnAction(func(ctx context.Context, cmd cli.Cmd, args []string) (err error) {
+		b := cmdr.Store().MustBool("debug")
+		println("debug flag: ", b)
+		if !b {
+			t.Fail()
+		}
+		println(cmdr.Set().Dump())
+
+		type manS struct {
+			Dir  string
+			Type int
+		}
+		type genS struct {
+			Manual manS
+		}
+		var v genS
+		err = cmdr.Set().GetSectionFrom("cmd.generate", &v)
+		if err != nil {
+			t.Fail()
+		}
+		if v.Manual.Type != 1 {
+			t.Fail()
+		}
+		return
+	}).
+		Build()
+	err := app.Run(ctx)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+}
+
+func TestTo(t *testing.T) {
+	ctx := context.Background()
+	app := cmdr.Create("app", "v1", `author`, `desc`,
+		cli.WithArgs("test-app", "--debug"),
+	).OnAction(func(ctx context.Context, cmd cli.Cmd, args []string) (err error) {
+		b := cmdr.Store().MustBool("debug")
+		println("debug flag: ", b)
+		if !b {
+			t.Fail()
+		}
+		println(cmdr.Set().Dump())
+
+		type manS struct {
+			Dir  string
+			Type int
+		}
+		type genS struct {
+			Manual manS
+		}
+		var v genS
+		err = cmdr.To("cmd.generate", &v)
+		if err != nil {
+			t.Fail()
+		}
+		if v.Manual.Type != 1 {
+			t.Fail()
+		}
+		return
+	}).
+		Build()
+	err := app.Run(ctx)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+}
+
 func TestExecNoRoot(t *testing.T) {
 	if err := cmdr.Exec(nil); !errors.Iss(err, cli.ErrEmptyRootCommand) {
 		t.Errorf("Error: %v", err)

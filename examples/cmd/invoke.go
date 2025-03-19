13e24ffb2ae01d3e302b45bdb8857594f94ac17d
Hedzr Yeh <hedzrz@gmail.com>
2025-03-19 08:51:52 +0800

    fix common examples/cmd/

--- examples/cmd/invoke.go
+++ examples/cmd/invoke.go
@@ -7,9 +7,9 @@ import (
 type invokeCmd struct{}
 
 func (invokeCmd) Add(app cli.App) {
-	app.Cmd("invoke").
+	app.Cmd("invoke").Description(`test invoke feature`).
 		With(func(b cli.CommandBuilder) {
-			b.Cmd("shell").InvokeShell(`ls -la`).UseShell("/bin/bash").OnAction(nil).Build()
-			b.Cmd("proc").InvokeProc(`say "hello, world!"`).OnAction(nil).Build()
+			b.Cmd("shell").Description(`invoke shell cmd`).InvokeShell(`ls -la`).UseShell("/bin/bash").OnAction(nil).Build()
+			b.Cmd("proc").Description(`invoke gui program`).InvokeProc(`say "hello, world!"`).OnAction(nil).Build()
 		})
 }
--- examples/cmd/preset-cmdline.go
+++ examples/cmd/preset-cmdline.go
@@ -10,13 +10,13 @@ type presetCmd struct{}
 
 func (presetCmd) Add(app cli.App) {
 	app.Cmd("preset", "p").
-		Description("preset command").
+		Description("preset command to inject into user input").
 		With(func(b cli.CommandBuilder) {
 			b.Flg("preset", "p").
 				Default(false).
 				Description("preset arg").
 				Build()
-			b.Cmd("cmd", "c").
+			b.Cmd("cmd", "c").Description("inject `-pv` into user input cmdline").
 				PresetCmdLines(`-pv`).
 				OnAction(func(ctx context.Context, cmd cli.Cmd, args []string) (err error) {
 					_, err = app.DoBuiltinAction(ctx, cli.ActionDefault)

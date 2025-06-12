996054e1d0e618fa8a7bfadeb42a731b6415555e
Hedzr Yeh <hedzrz@gmail.com>
2025-06-12 08:27:28 +0800

    add doc to `Create`

--- setup_app.go
+++ setup_app.go
@@ -11,6 +11,44 @@ import (
 )
 
 // Create provides a concise interface to create an cli app easily.
+//
+// See also [New].
+//
+// A tiny sample app can be:
+//
+//	package main
+//
+//	import (
+//		"context"
+//		"os"
+//
+//		"github.com/hedzr/cmdr/v2"
+//		"github.com/hedzr/cmdr/v2/examples/cmd"
+//		"github.com/hedzr/cmdr/v2/pkg/logz"
+//	)
+//
+//	const (
+//		appName = "concise"
+//		desc    = `concise version of tiny app.`
+//		version = cmdr.Version
+//		author  = `The Example Authors`
+//	)
+//
+//	func main() {
+//		app := cmdr.Create(appName, version, author, desc).
+//			WithAdders(cmd.Commands...).
+//			Build()
+//
+//		ctx, cancel := context.WithCancel(context.Background())
+//		defer cancel()
+//
+//		if err := app.Run(ctx); err != nil {
+//			logz.ErrorContext(ctx, "Application Error:", "err", err) // stacktrace if in debug mode/build
+//			os.Exit(app.SuggestRetCode())
+//		} else if rc := app.SuggestRetCode(); rc != 0 {
+//			os.Exit(rc)
+//		}
+//	}
 func Create(appName, version, author, desc string, opts ...cli.Opt) Creator {
 	s := &cs{}
 	return s.Create(appName, version, author, desc, opts...)

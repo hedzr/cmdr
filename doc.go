73b122afdfcae74a70b12ae62340d5651e319b20
Hedzr Yeh <hedzrz@gmail.com>
2025-07-06 06:40:12 +0800

    bump to v2.1.39

--- doc.go
+++ doc.go
@@ -119,4 +119,4 @@
 // For more documentation please go and check out https://docs.hedzr.com/cmdr.v2/.
 package cmdr
 
-const Version = "v2.1.38" // Version fir hedzr/cmdr/v2
+const Version = "v2.1.39" // Version fir hedzr/cmdr/v2
